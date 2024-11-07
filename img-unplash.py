1855f4ae8f6fb618698b04ca854a26a97d3a288d
Ryan Vieira <108676672+IcyHoot@users.noreply.github.com>
2024-11-07 01:12:18 -0300

    Update img-unplash.py
    changed the interface to work with "messagebox" , "simpledialog" and "filedialog"

--- img-unplash.py
+++ img-unplash.py
@@ -2,12 +2,12 @@
 # API in order to download images based on a specific search query (in this case,
 # 'pessoas' which means 'people'). Here's a breakdown of what the script is doing:
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, simpledialog, filedialog
 
 import requests
 import threading
 import os
-
+import json
 
 entry_filtro = None
 entry_dest = None
@@ -15,7 +15,9 @@ root = tk.Tk()
 api_key = 't3aoXTl1h2BD9vtMi8Fv_wP2mTshMUUff6w6Cj-zLpY' # a sua API KEY do Unsplash 
 filtro = '' # oq vai pesquisar
 dest = '' # onde vai salvar as imgs
-t_img = 100
+t_img = ''
+passw = ''
+max_t = 3
 i_pg = 30
 
 # url q ele vai utilizar para buscar as imgs
@@ -23,44 +25,68 @@ url = f'https://api.unsplash.com/search/photos?query={filtro}&client_id={api_key
 
 response = requests.get(url)
 
+def jsonsaver():
+    root.withdraw()
+    config = {'password': passw, 'tentativas': max_t}
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
+
+def jsonloader():
+    root.withdraw()
+    global passw, max_t
+    try:
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+            passw = config.get('password', '0000')
+            max_t = config.get('tentativas', 3)
+    except FileNotFoundError:
+        passw = simpledialog.askstring('Image Downloader','Arquivo de Configurações não encontrado! \nDigite uma senha: ')
+        return
+
 def passUser(): # função de verif
     root.withdraw()
-    passw = 'unsplash'
     t = 0
     while True:
         pw = simpledialog.askstring('Image Downloader', 'Digite a sua senha: ')
         if pw:
             if pw == passw:
                 messagebox.showinfo('Image Downloader', 'SENHA CORRETA. \nIniciando o sistema.')
-                download()
+                main()
                 break
             else:
                 t += 1
                 messagebox.showwarning('Image Downloader',f'SENHA INCORRETA. \nTentativa: {t}')
-                if t == 3:
+                if t == max_t:
                     messagebox.showwarning('Image Downloader','Tente novamente mais tarde.')
                     root.quit()
                     break
         else:
             messagebox.showwarning('Image Downloader','Insira uma senha!')
 
+def main():
+    global filtro, dest, t_img
+    filtro = simpledialog.askstring('Image Downloader','Insira o Filtro: ')
+    print('Filtro: ', filtro)
+    messagebox.showinfo('Image Downloader','Escolha o diretório')
+    dest = filedialog.askdirectory(title='Escolha o diretório.')
+    print('Destino: ', dest)
+    t_img = simpledialog.askinteger('Image Downloader', 'Insira a Quantidade: ')
+    print('Quantidade: ', t_img)
+    if filtro is not None and t_img is not None and dest is not None:
+        download()
+
 def mkdir(dest): # função que cria o destino se for falso
-    if not os.path.exists(dest):
-        os.makedirs(dest)
+        if not os.path.exists(dest):
+            os.makedirs(dest)
 
 def thread():
     threading.Thread(target=download())
     threading.Thread(target=passUser())
+    threading.Thread(target=main())
 
 def download(): # inicia o download das imagens com base no dados fornecidos
-    global api_key, filtro, dest
-
-    filtro = entry_filtro.get()
-    dest = entry_dest.get()
-
-    mkdir(dest)
-
-    for page in range(1, (t_img // i_pg) + 2): # +2 pra garantir q vai pegar 100 imagens
+    t_Img = int(t_img)
+    for page in range(1, (t_Img // i_pg) + 2): # +2 pra garantir q vai pegar 100 imagens
         url = f'https://api.unsplash.com/search/photos?query={filtro}&client_id={api_key}&per_page={i_pg}&page={page}'
         response = requests.get(url)
 
@@ -81,30 +107,13 @@ def download(): # inicia o download das imagens com base no dados fornecidos
                     handler.write(img_data)
                 print(f'imagem {file_number + 1} baixada.')
                 
-                if file_number + 1 >= t_img:
-                    print('download concluido, sobre: ', filtro)
+                if file_number + 1 >= t_Img:
+                    messagebox.showinfo('Image Downloader',f'Download finalizado! \n\nFiltro: {filtro} \nDestino: {dest} \nTotal de Imagens: {t_Img}')
                     break
         else:
             print('Nenhuma imagem encontrada ou chave "results" nao encontrada.')
             print(data)
 
-def submit():
-    passUser()
-
-def main():
-    global entry_filtro, entry_dest
-
-    tk.Label(root, text='Filtro:').grid(row=0, column=0, padx=10, pady=5)
-    entry_filtro = tk.Entry(root)
-    entry_filtro.grid(row=0, column=1, padx=10, pady=5)
-
-    tk.Label(root, text='Destino:').grid(row=1, column=0, padx=10, pady=5)
-    entry_dest = tk.Entry(root)
-    entry_dest.grid(row=1, column=1, padx=10, pady=5)
-
-    button_iniciar = tk.Button(root, text="Iniciar Download", command=submit)
-    button_iniciar.grid(row=3, columnspan=2, pady=10)
-
-if __name__ == '__main__':
-    main()
-    root.mainloop()
+jsonloader()
+jsonsaver()
+passUser()
